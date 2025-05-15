8d14733de416da268a839471a846f01748fdef6d
Dangar-dev <danielsobraldev@gmail.com>
2025-05-15 14:05:04 -0300

    Criando os codigos do repositorios login e produto

--- DragonFantasy/Repositorio/LoginRepositorio.cs
+++ DragonFantasy/Repositorio/LoginRepositorio.cs
@@ -1,10 +1,43 @@
-﻿using MySql.Data.MySqlClient;
+﻿using DragonFantasy.Models;
+using MySql.Data.MySqlClient;
 using System.Data;
 
 
 namespace DragonFantasy.Repositorio
 {
-    public class LoginRepositorio
+    public class LoginRepositorio(IConfiguration configuration)
     {
+
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+
+        public Usuario ObterUsuario(string email)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new("SELECT * FROM Usuario WHERE Email = @email", conexao);
+
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    Usuario usuario = null;
+
+                    if (dr.Read())
+                    {
+                        usuario = new Usuario
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = dr["Nome"].ToString(),
+                            Email = dr["Email"].ToString(),
+                            Senha = dr["Senha"].ToString()
+
+                        };
+                    }
+                    return usuario;
+                }
+            }
+        }
     }
 }
